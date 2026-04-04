6f2dfaab24ee96efa95323f2c82edb91bb731762
unknown <janavikorgaonkar@gmail.com>
2026-04-04 15:37:49 +0530

    a

---
+++ Menu/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant_Menu.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok("Alive");
+        }
+    }
+}
